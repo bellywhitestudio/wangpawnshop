0ebacc1c55cf57d1fcd5f2a72efbcf06a223dc21
jennifer.juan <jennifer.juan@meet.jobs>
2019-11-25 03:07:18 +0800

    add ticket's function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,37 @@
 // &filterByFormula=Display
+let active_index = 0;
+let tikets = [];
+
+window.addEventListener('load', function () {
+    document.querySelector('#button-next-ticket').addEventListener('click', function () {
+        if(active_index === tikets.length - 1) {
+            active_index = 0;
+        } else {
+            active_index += 1;
+        }
+        refresh_tick();
+    });
+    document.querySelector('#button-prev-ticket').addEventListener('click', function () {
+        if(active_index === 0) {
+            active_index = tikets.length - 1;
+        } else {
+            active_index -= 1;
+        }
+        refresh_tick();
+    });
+}, false);
+
+function refresh_tick() {
+    let dropped = document.querySelector('#dropped');
+    let exchanged = document.querySelector('#exchanged');
+    let reason = document.querySelector('#reason');
+    dropped.innerHTML = tikets[active_index].fields['Dropped Object'];
+    exchanged.innerHTML = tikets[active_index].fields['Exchange Object'];
+    reason.innerHTML = tikets[active_index].fields['Reason'];
+}
+
 axios.get('https://api.airtable.com/v0/applgpGFRXcIF7ORT/Data?api_key=key1Wb5cOe9qMbz4N&fields%5B%5D=Name&fields%5B%5D=Dropped+Object&fields%5B%5D=Exchange+Object&fields%5B%5D=Reason')
     .then((response) => {
-        const raw_records = response.data.records;
-        let slides = document.querySelector('.carousel-inner');
-        for(let i=0; i<raw_records.length; i++) {
-            let item = document.createElement("div");
-            item.classList.add("item");
-            if(i===0) item.classList.add("active");
-            let ticket = document.createElement("div");
-            ticket.classList.add("ticket");
-            let name = document.createElement("h2");
-            name.innerHTML = raw_records[i].fields['Name'];
-            let dropped = document.createElement("p");
-            dropped.innerHTML = `我願意拿出 ${raw_records[i].fields['Dropped Object']} ，`;
-            let exchanged = document.createElement("p");
-            exchanged.innerHTML = `為了換得 ${raw_records[i].fields['Exchange Object']} ，`;
-            let reason = document.createElement("p");
-            reason.innerHTML = `因為 ${raw_records[i].fields['Reason']} 。`;
-            ticket.appendChild(name);
-            ticket.appendChild(dropped);
-            ticket.appendChild(exchanged);
-            ticket.appendChild(reason);
-            item.appendChild(ticket);
-            slides.appendChild(item);
-        }
-    })
+        tikets = response.data.records;
+        refresh_tick();
+    });
