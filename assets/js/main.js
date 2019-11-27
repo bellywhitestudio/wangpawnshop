d7bd0a06b0998d397376ab7f209bf1921c807638
jennifer.juan <jennifer.juan@meet.jobs>
2019-11-27 20:27:37 +0800

    add slider for tickets

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,44 +1,66 @@
-// &filterByFormula=Display
-let active_index = 0;
-let tikets = [];
-
-axios.get('https://api.airtable.com/v0/applgpGFRXcIF7ORT/Data?api_key=key1Wb5cOe9qMbz4N&fields%5B%5D=Name&fields%5B%5D=Dropped+Object&fields%5B%5D=Exchange+Object&fields%5B%5D=Reason')
-    .then((response) => {
-        tikets = response.data.records;
-        refresh_tick();
-    });
-
 window.addEventListener('load', function () {
-    document.querySelector('#button-next-ticket').addEventListener('click', function () {
-        if(active_index === tikets.length - 1) {
-            active_index = 0;
-        } else {
-            active_index += 1;
-        }
-        refresh_tick();
-    });
-    document.querySelector('#button-prev-ticket').addEventListener('click', function () {
-        if(active_index === 0) {
-            active_index = tikets.length - 1;
-        } else {
-            active_index -= 1;
-        }
-        refresh_tick();
-    });
-
     document.querySelector('#button-scroll-to-tickets').addEventListener('click', function () {
         const section_tickets = document.querySelector('section.tickets');
         if(section_tickets) page_smoothly_scroll_to(section_tickets.offsetTop, 1000);
     });
+
+    axios.get('https://api.airtable.com/v0/applgpGFRXcIF7ORT/Data?api_key=key1Wb5cOe9qMbz4N&fields%5B%5D=Name&fields%5B%5D=Dropped+Object&fields%5B%5D=Exchange+Object&fields%5B%5D=Reason&maxRecords=20')
+        .then((response) => {
+            const tickets = response.data.records;
+            refresh_tickets(tickets);
+        });
+
+
 }, false);
 
-function refresh_tick() {
-    let dropped = document.querySelector('#dropped');
-    let exchanged = document.querySelector('#exchanged');
-    let reason = document.querySelector('#reason');
-    dropped.innerHTML = tikets[active_index].fields['Dropped Object'];
-    exchanged.innerHTML = tikets[active_index].fields['Exchange Object'];
-    reason.innerHTML = tikets[active_index].fields['Reason'];
+function refresh_tickets(tickets) {
+    let sliders = document.querySelector('#tickets-slider ul.glide__slides');
+    let lines = [{
+        field_name: 'Dropped Object',
+        label_name: "我願意拿出"
+    }, {
+        field_name: 'Exchange Object',
+        label_name: "為了換得"
+    }, {
+        field_name: 'Reason',
+        label_name: "因為"
+    }];
+    for(let i=0; i<tickets.length; i++) {
+        let slider = document.createElement("li");
+        slider.className = 'glide__slide ticket-wrapper';
+        let img = document.createElement('img');
+        img.src = './assets/img/ticket.svg';
+        let content = document.createElement('div');
+        content.className = "content";
+
+        for(let j=0; j<lines.length; j++) {
+            let line = document.createElement('div');
+            line.className = "line";
+            let label = document.createElement('span');
+            label.className = "label";
+            label.innerText = lines[j].label_name;
+            let text = document.createElement('span');
+            text.className = "text";
+            text.innerText = tickets[i].fields[lines[j].field_name];
+
+            line.appendChild(label);
+            line.appendChild(text);
+            content.appendChild(line);
+        }
+
+        slider.appendChild(img);
+        slider.appendChild(content);
+        sliders.appendChild(slider);
+    }
+
+    let glide = new Glide('#tickets-slider', {
+        type: 'carousel',
+        perView: 1,
+        autoplay: 3000,
+        rewind: true,
+        hoverpause: true
+    });
+    glide.mount();
 }
 
 function smoothly_scroll_to(element, to, duration) {
